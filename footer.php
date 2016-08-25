d01abb5ccfffc8c5274bfac3fb6bad6770bc2d7f
Ryan Mueller <rmill865@gmail.com>
2016-08-24 21:42:24 -0400

    rearranged the header links

--- footer.php
+++ footer.php
@@ -8,6 +8,7 @@
         <h4>
             <a href="mailto:cbcomic@gmail.com"><i class="fa fa-envelope-o" aria-hidden="true"></i></a>
 	    <a href="https://www.facebook.com/cbcomic/"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+            <a href="https://twitter.com/khialest"><i class="fa fa-twitter" aria-hidden="true"></i></a>
 	    <a href="http://cb-comic.tumblr.com/"><i class="fa fa-tumblr" aria-hidden="true"></i></a>
         </h4>
     </div>
--- header.php
+++ header.php
@@ -23,10 +23,10 @@
 <div class="header-links header">
   <h2>
   <a href="/">Read the Comic</a> -
-  <a href="<?php echo get_page_link(31); ?>">About</a> -
-  <a href="<?php echo get_page_link(20); ?>">Support the Series</a> - 
-  <a href="http://cb-comic.com/soundtrack">Soundtrack</a> -
-  <a href="http://cb-comic.tumblr.com">Tumblr</a> - 
-  <a href="https://www.patreon.com/cbcomic">Patreon</a>
+  <a href="<?php echo get_page_link(31); ?>">About</a> - 
+  <a href="<?php echo get_page_link(178); ?>">Buy the Comic</a> - 
+  <a href="https://www.patreon.com/cbcomic">Patreon</a> - 
+  <a href="https://cb-comic.com/soundtrack">Soundtrack</a> - 
+  <a href="https://redbubble.com/people/cbcomic">CB Store</a>
   </h2>
 </div>
